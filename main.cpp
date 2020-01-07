1d6f75fa03fde594e5a0937a6271c4f36bda12f2
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2020-01-07 16:23:03 +0700

    update

--- main.cpp
+++ main.cpp
@@ -3,12 +3,50 @@
 #include <cmath>
 #include <cstdlib>
 #include <conio.h>
+#include <ctime>
 
 using namespace std;
 
+void sorting(string arr[], int n)
+{
+    int i, j;
+    string key;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// A utility function to print an array of size n
+void cetak(string arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << endl;
+    cout << endl;
+}
+
 void tarik_tunai(int line2,int cash,int cash1){
+    time_t now = time(0);
+	tm *ltm = localtime(&now);
+    string bulan[]={"januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember",};
+    int x;
+    int tanggal;
+    string month;
+    int tahun;
     ifstream outa;
-    outa.open("file2.txt");
+    outa.open("saldo.txt");
     if(outa.is_open())
     {
         while(!outa.eof())
@@ -16,6 +54,10 @@ void tarik_tunai(int line2,int cash,int cash1){
             outa >> line2;
             cout << "Masukan jumlah uang : ";
             cin >> cash;
+            x=1 + ltm->tm_mon;
+            month=bulan[x-1];
+            tanggal=ltm->tm_mday;
+            tahun=1900 + ltm->tm_year;
             if(cash>line2)
             {
                 cout << "Maaf transaksi gagal";
@@ -27,16 +69,17 @@ void tarik_tunai(int line2,int cash,int cash1){
                 cout << "Terima Kasih" << endl << endl;
                 outa.close();
                 ofstream inb;
-                inb.open("file2.txt");
+                inb.open("saldo.txt");
                 if(inb.is_open())
                 {
                     inb << cash1;
                 }
                 inb.close();
                 fstream inx;
-                inx.open("file3.txt", ios::app);
+                inx.open("riwayat_tarik.txt", ios::app);
                 if(inx.is_open())
                 {
+                    inx << "\tTanggal " << tanggal << " " << month << " " << tahun << endl;
                     inx << endl << "Rp. " << cash << endl;
                 }
                 inx.close();
@@ -46,8 +89,21 @@ void tarik_tunai(int line2,int cash,int cash1){
 }
 
 void setor_tunai(int line2,int cash2,int cash3){
+    time_t now = time(0);
+	tm *ltm = localtime(&now);
+    string bulan[]={"januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember",};
+    int x;
+    int tanggal;
+    string month;
+    int tahun;
+
+    x=1 + ltm->tm_mon;
+    month=bulan[x-1];
+    tanggal=ltm->tm_mday;
+    tahun=1900 + ltm->tm_year;
+
     ifstream outa;
-    outa.open("file2.txt");
+    outa.open("saldo.txt");
     if(outa.is_open())
     {
         while(!outa.eof())
@@ -61,16 +117,17 @@ void setor_tunai(int line2,int cash2,int cash3){
             cout << "Terima Kasih" << endl;
             outa.close();
             ofstream q;
-            q.open("file2.txt");
+            q.open("saldo.txt");
             if(q.is_open())
             {
                 q << cash3;
             }
             q.close();
             fstream inx;
-            inx.open("file4.txt", ios::app);
+            inx.open("riwayat_setor.txt", ios::app);
             if(inx.is_open())
             {
+                inx << "\tTanggal " << tanggal << " " << month << " " << tahun << endl;
                 inx << endl << "Rp. " << cash2 << endl;
             }
             inx.close();
@@ -79,7 +136,7 @@ void setor_tunai(int line2,int cash2,int cash3){
 }
 
 void riwayat_tarik_tunai(){
-    ifstream f("file3.txt");
+    ifstream f("riwayat_tarik.txt");
     if(f.is_open())
     cout << endl << f.rdbuf();
 
@@ -87,7 +144,7 @@ void riwayat_tarik_tunai(){
 }
 
 void riwayat_setor_tunai(){
-    ifstream f("file4.txt");
+    ifstream f("riwayat_setor.txt");
     if(f.is_open())
     cout << endl << f.rdbuf();
     f.close();
@@ -96,7 +153,7 @@ void riwayat_setor_tunai(){
 void info(){
     int line2;
     ifstream outo;
-    outo.open("file2.txt");
+    outo.open("saldo.txt");
     if(outo.is_open())
     {
         while(!outo.eof())
@@ -112,10 +169,15 @@ void info(){
 int main()
 {
     main:
+    string bulan[]={"januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember",};
+    string xxx;
     string x;
     string line;
     string line4;
     string search;
+    string buffer;
+    string a;
+    int xx;
     int cek;
     int cash;
     int cash1;
@@ -138,7 +200,7 @@ int main()
     cout << "=============================\n";
 
     ifstream outf;
-    outf.open("file.txt");
+    outf.open("pin.txt");
     cout << "Masukan 4 digit pin : ";
     cin >> pin;
 
@@ -155,13 +217,16 @@ int main()
                     menu:
                     cout << endl;
                     cout << "=========================\n";
+                    cout << "       ATM BANGKIT    \n";
+                    cout << "=========================\n";
                     cout << "1. Tarik Tunai\n";
                     cout << "2. Riwayat Tarik tunai\n";
                     cout << "3. Setor Tunai\n";
                     cout << "4. Riwayat Setor Tunai\n";
                     cout << "5. Info saldo\n";
                     cout << "6. Bayar Kuliah\n";
-                    cout << "7. Keluar\n";
+                    cout << "7. Lokasi ATM\n";
+                    cout << "8. Keluar\n";
                     cout << "Pilih = ";
                     cin >> c;
                     cout << endl;
@@ -180,35 +245,73 @@ int main()
                         {
                             system("CLS");
                             cout << "1. Tampilkan Semua Riwayat Tarik Tunai\n";
-                            cout << "2. Cari Riwayat Tarik Tunai\n";
+                            cout << "2. Cari Riwayat Tarik Tunai Berdasarkan bulan\n";
+                            cout << "3. Cari Riwayat Tarik Tunai Berdasarkan tahun\n";
                             cout << "Pilih : ";
                             cin >> pilih;
-
-                            if(pilih==1){
+                            switch(pilih){
+                            case 1:{
                                 riwayat_tarik_tunai();
                                 goto coba;
-                            }else if(pilih==2){
+                                break;
+                            }
+                            case 2:{
+                                system("CLS");
                                 ifstream file;
-                                file.open("file3.txt");
-                                cout << "Cari : ";
-                                cin >> search;
-
+                                file.open("riwayat_tarik.txt");
+                                cout << "Cari bulan ke - : ";
+                                cin >> xx;
+                                xxx=bulan[xx-1];
                                 if(file.is_open()){
-                                    unsigned int curLine = 0;
-                                    while(!file.eof()){
-                                        curLine++;
-                                        getline(file,line);
-                                        if((offset = line.find(search,0))!= string::npos){
-                                            cout << endl << "Ditemukan " << search << " di Baris ke : " << curLine << endl;
+                                    while(getline(file,line)){
+                                        if (line.find(xxx, 0) != string::npos){
+                                            cout << endl;
+                                            cout << "\t\t" << line << endl;
+                                            cout << "\t\t======================================\n";
+                                            getline(file,buffer);
+                                            a.append(buffer);
+                                            getline(file,buffer);
+                                            a.append("\n"+buffer);
+                                            cout << buffer << endl;
+                                            cout << "=============\n";
                                         }
                                     }
                                     file.close();
                                     goto coba;
-                                }else{
-                                    cout << "Tidak bisa buka file" << endl;
-                                    return 0;
+                                    break;
                                 }
+                            }case 3:{
+                                system("CLS");
+                                ifstream file;
+                                file.open("riwayat_tarik.txt");
+                                cout << "Cari berdasarkan tahun - : ";
+                                cin >> xxx;
+                                if(file.is_open()){
+                                    while(getline(file,line)){
+                                        if (line.find(xxx, 0) != string::npos){
+                                            cout << endl;
+                                            cout << "\t\t" << line << endl;
+                                            cout << "\t\t======================================\n";
+                                            getline(file,buffer);
+                                            a.append(buffer);
+                                            getline(file,buffer);
+                                            a.append("\n"+buffer);
+                                            cout << buffer << endl;
+                                            cout << "=============\n";
+                                        }
+                                    }
+                                    file.close();
+                                    a = "0";
+                                    goto coba;
+                                    break;
+                                }
+                            }case 4:{
+                                cout << "Tidak bisa buka file" << endl;
+                                return 0;
+                                break;
                             }
+                            }
+
 
                             break;
                         }
@@ -219,39 +322,78 @@ int main()
                             goto coba;
                             break;
                         }
-                        case 4:
+                    case 4:
                         {
                             system("CLS");
-                            cout << "1. Tampilkan Semua Riwayat Setor Tunai\n";
-                            cout << "2. Cari Riwayat Setor Tunai\n";
+                            cout << "1. Tampilkan Semua Riwayat Tarik Tunai\n";
+                            cout << "2. Cari Riwayat Setor Tunai Berdasarkan bulan\n";
+                            cout << "3. Cari Riwayat Setor Tunai Berdasarkan tahun\n";
                             cout << "Pilih : ";
                             cin >> pilih;
-
-                            if(pilih==1){
-                                riwayat_setor_tunai();
+                            switch(pilih){
+                            case 1:{
+                                riwayat_tarik_tunai();
                                 goto coba;
-                            }else if(pilih==2){
+                                break;
+                            }
+                            case 2:{
+                                system("CLS");
                                 ifstream file;
-                                file.open("file4.txt");
-                                cout << "Cari : ";
-                                cin >> search;
-
+                                file.open("riwayat_setor.txt");
+                                cout << "Cari bulan ke - : ";
+                                cin >> xx;
+                                xxx=bulan[xx-1];
                                 if(file.is_open()){
-                                    unsigned int curLine = 0;
-                                    while(!file.eof()){
-                                        curLine++;
-                                        getline(file,line);
-                                        if((offset = line.find(search,0))!= string::npos){
-                                            cout << endl << "Ditemukan " << search << " di Baris ke : " << curLine << endl;
+                                    while(getline(file,line)){
+                                        if (line.find(xxx, 0) != string::npos){
+                                            cout << endl;
+                                            cout << "\t\t" << line << endl;
+                                            cout << "\t\t======================================\n";
+                                            getline(file,buffer);
+                                            a.append(buffer);
+                                            getline(file,buffer);
+                                            a.append("\n"+buffer);
+                                            cout << buffer << endl;
+                                            cout << "=============\n";
                                         }
                                     }
                                     file.close();
                                     goto coba;
-                                }else{
-                                    cout << "Tidak bisa buka file" << endl;
-                                    return 0;
+                                    break;
                                 }
+                            }case 3:{
+                                system("CLS");
+                                ifstream file;
+                                file.open("riwayat_setor.txt");
+                                cout << "Cari berdasarkan tahun - : ";
+                                cin >> xxx;
+                                if(file.is_open()){
+                                    while(getline(file,line)){
+                                        if (line.find(xxx, 0) != string::npos){
+                                            cout << endl;
+                                            cout << "\t\t" << line << endl;
+                                            cout << "\t\t======================================\n";
+                                            getline(file,buffer);
+                                            a.append(buffer);
+                                            getline(file,buffer);
+                                            a.append("\n"+buffer);
+                                            cout << buffer << endl;
+                                            cout << "=============\n";
+                                        }
+                                    }
+                                    file.close();
+                                    a = "0";
+                                    goto coba;
+                                    break;
+                                }
+                            }case 4:{
+                                cout << "Tidak bisa buka file" << endl;
+                                return 0;
+                                break;
                             }
+                            }
+
+
                             break;
                         }
                         case 5:
@@ -277,7 +419,7 @@ int main()
                         case 6:
                         {
                             ifstream outa;
-                            outa.open("file2.txt");
+                            outa.open("saldo.txt");
                                 if(outa.is_open())
                                 {
                                     while(!outa.eof())
@@ -295,7 +437,7 @@ int main()
                                             if (x=="y" || x=="Y")
                                             {
                                                 ofstream bayar;
-                                                bayar.open("file2.txt");
+                                                bayar.open("saldo.txt");
                                                 total= line2-5000000;
                                                 if (bayar.is_open())
                                                 {
@@ -324,8 +466,15 @@ int main()
                                 }
                                 break;
                         }
-
-                        case 7:
+                        case 7:{
+                            string arr[]={"Purwokerto","Bumiayu","Banyumas","Cilacap","Kebumen","Tegal"};
+                            int n = sizeof(arr)/sizeof(*arr);
+                            sorting(arr,n);
+                            cetak(arr,n);
+                            goto coba;
+                            break;
+                        }
+                        case 8:
                         {
                             system("CLS");
                             cout << "Terima Kasih" << endl;
@@ -343,7 +492,7 @@ int main()
                         }
                     }
                 }
-                while(c!=7);
+                while(c!=8);
         }
         else
         {
